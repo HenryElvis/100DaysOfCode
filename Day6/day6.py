cd6f81aa08d8d642a9e51e2443bf8c36cf6baf75
Elvis <elvishenry2402@gmail.com>
2022-08-02 17:42:23 +0200

    Maze code

--- Day6/day6.py
+++ Day6/day6.py
@@ -3,17 +3,16 @@ def turn_right():
     turn_left()
     turn_left()
 
+while front_is_clear():
+    move()
+
+turn_left()
+    
 while not at_goal():
     if right_is_clear():
         turn_right()
-        
-        if front_is_clear():
-            move()
-        else:
-            turn_right()
-            
-    elif not right_is_clear():
-        if front_is_clear():
-            move()
-        else:
-            turn_left()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
