3e8772f60f70020133c3c0dcd21c193105ffe2fe
Elvis <elvishenry2402@gmail.com>
2022-08-04 22:56:01 +0200

    Day 8 Caeser cipher

---
+++ Day8/day8.py
@@ -0,0 +1,46 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+logo = """           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+"""
+
+print(logo)
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+
+if direction != "encode" and direction != "decode":
+	quit()
+
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+def Caesar(plain_text, shift_amount, cipher_direction):
+
+	new_text = ""
+	
+	for letter in plain_text:
+		if cipher_direction == "encode":		
+			position = alphabet.index(letter) + shift_amount
+		else:
+			position = alphabet.index(letter) - shift_amount
+
+		new_letter = alphabet[position]
+		
+		new_text += new_letter
+	print(f"Your new text is : '{new_text}'")
+
+Caesar(text, shift, direction)
