01f2b56d092255083d750105cccd3ccfaaeb6143
Elvis <elvishenry2402@gmail.com>
2022-08-22 18:57:24 +0200

    add feature

--- Day25/day25.py
+++ Day25/day25.py
@@ -22,20 +22,26 @@ game_is_on = True
 score = 0
 title = "Guess the State"
 
+states_guessed = []
+
 while game_is_on:
     if score > 0:
         title = f"{score}/50 States Correct"
 
-    answer = screen.textinput(title=title, prompt="What's another state's name ?")
+    answer = screen.textinput(title=title, prompt="What's another state's name ?").capitalize()
 
-    if answer == "q":
+    if answer == "Q":
         game_is_on = False
+        break
 
-    for state in data_states:
-        if state == answer.capitalize():
-            current_state = data[data.state == state]
-            t.goto(int(current_state.x), int(current_state.y))
-            t.write(state)
-            score += 1
+    if answer not in states_guessed:
+        for state in data_states:
+            if state == answer:
+                current_state = data[data.state == state]
+                t.goto(int(current_state.x), int(current_state.y))
+                t.write(state)
+                score += 1
+                states_guessed.append(answer)
+        print(states_guessed)
 
 # screen.exitonclick()
