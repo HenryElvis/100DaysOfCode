10749bfd9c3ed24dee33359b82bfa25285147229
Elvis <elvishenry2402@gmail.com>
2022-08-31 18:51:31 +0200

    Day34 - Quizzler API Tkinter

---
+++ Day34/data.py
@@ -0,0 +1,11 @@
+import requests
+
+params = {
+    "amount":10,
+    "type":"boolean"
+}
+
+request = requests.get(url="https://opentdb.com/api.php", params=params)
+request.raise_for_status()
+
+question_data = request.json()["results"]
