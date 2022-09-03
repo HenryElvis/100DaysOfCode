6f175a7705caea18a9f9d3729c1f3f3b443472c3
Elvis <elvishenry2402@gmail.com>
2022-09-03 21:20:27 +0200

    End

--- Day36/day.py
+++ Day36/day.py
@@ -1,4 +1,6 @@
 import requests
+import os
+from twilio.rest import Client
 
 STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
@@ -6,8 +8,8 @@ COMPANY_NAME = "Tesla Inc"
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 
-# KEY = "7b4ac2606c474881bd3181f8f3568aae"
-KEY = "Z5SQPCA4MQ9U8S6N"
+NEW_API_KEY = os.environ.get("API_KEY_NEWSAPI")
+KEY = os.environ.get("API_KEY_ALPHA_AVANTAGE")
 
 PARAMS = {
     "function": "TIME_SERIES_DAILY",
@@ -15,36 +17,46 @@ PARAMS = {
     "apikey": KEY
 }
 
+ACCOUNT_SID = "ACd7b9aab0b9f3548d1077c4d18fd5a99f"
+AUTH_TOKKEN = os.environ.get("AUTH_TOKKEN_TWILLIO")
+TEL = "+16187643829"
+
 request = requests.get(url=STOCK_ENDPOINT, params=PARAMS)
 request.raise_for_status()
 
-print(request.json())
+data = request.json()["Time Series (Daily)"]
 
-## STEP 1: Use https://newsapi.org/docs/endpoints/everything
-# When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
-#HINT 1: Get the closing price for yesterday and the day before yesterday. Find the positive difference between the two prices. e.g. 40 - 20 = -20, but the positive difference is 20.
-#HINT 2: Work out the value of 5% of yerstday's closing stock price. 
+data_list = [value for key, value in data.items()]
 
+yesterday_data = data_list[0]
+yesterday_closing_price = yesterday_data["4. close"]
 
-## STEP 2: Use https://newsapi.org/docs/endpoints/everything
-# Instead of printing ("Get News"), actually fetch the first 3 articles for the COMPANY_NAME. 
-#HINT 1: Think about using the Python Slice Operator
+day_before_yesterday = data_list[1]
+day_before_yesterday_closing_price = day_before_yesterday["4. close"]
 
+difference = (abs(float(yesterday_closing_price) - float(day_before_yesterday_closing_price))) / float(yesterday_closing_price) * 100
 
-## STEP 3: Use twilio.com/docs/sms/quickstart/python
-# Send a separate message with each article's title and description to your phone number. 
-#HINT 1: Consider using a List Comprehension.
+if difference > 2:
+    PARAMS = {
+        "apiKey": NEW_API_KEY,
+        "qInTitle": COMPANY_NAME
+    }
 
+    request = requests.get(url=NEWS_ENDPOINT, params=PARAMS)
+    request.raise_for_status()
 
+    articles = request.json()["articles"][:3]
 
-#Optional: Format the SMS message like this: 
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
+    formatted_articles = [f"Headline: {article['title']}.\nBrief: {article['description']}.\nURL: {article['url']}." for article in articles]
 
+    client = Client(ACCOUNT_SID, AUTH_TOKKEN)
+    
+    for article in formatted_articles:
+        message = client.messages \
+            .create(
+                body= article,
+                from_=TEL,
+                to="+33 7 68 94 94 89"
+            )
+
+        print(message.status)
