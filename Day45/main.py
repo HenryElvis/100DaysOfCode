86eb2e828863bb82104471b99e1bb3b9e1306d5e
Elvis <elvishenry2402@gmail.com>
2022-12-12 22:32:08 +0100

    Day45
    Done

--- Day45/main.py
+++ Day45/main.py
@@ -1,5 +1,4 @@
 from bs4 import BeautifulSoup
-#import lxml
 import requests
 
 with open("website.html", encoding="utf-8") as file:
@@ -16,5 +15,17 @@ first_a = soup.select_one(selector="p a")
 data = requests.get("https://henryelvis.fr/")
 page = BeautifulSoup(data.text, "html.parser")
 
-print(page.title.string)
-print(page.select_one(selector="a").get("href"))
+#print(page.title.string)
+#print(page.select_one(selector="a").get("href"))
+
+URL = requests.get("https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/")
+website = URL.text
+soup = BeautifulSoup(website, "html.parser")
+
+titles = soup.find_all(name="h3", class_="title")
+titles = [title.getText() for title in titles]
+titles = titles[::-1]
+
+with open("movies.txt", mode="w", encoding="UTF-8") as file:
+    for title in range(len(titles)):
+        file.write(f"{titles[title]}\n")
