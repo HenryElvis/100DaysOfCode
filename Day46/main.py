e9dc88ec213ce3aa53201815beb47542c4fa0453
Elvis <elvishenry2402@gmail.com>
2022-12-13 13:03:42 +0100

    FirstStep

--- Day46/main.py
+++ Day46/main.py
@@ -1,19 +1,26 @@
 import requests
 from bs4 import BeautifulSoup
 
-requests = requests.get("https://www.timeout.com/film/best-anime-movies")
-requests = requests.text
-soup = BeautifulSoup(requests, "html.parser")
+response = requests.get("https://www.timeout.com/film/best-anime-movies")
+response = response.text
+soup = BeautifulSoup(response, "html.parser")
 
 animes = soup.select("h3")
 anime = [anime.getText() for anime in animes]
 
-for i in range(len(anime)):
-    print(anime[i])
+#for i in range(len(anime)):
+#    print(anime[i])
 
-date = input("Enter a date in the format YYYY-MM-DD: ")
+#date = input("Enter a date in the format YYYY-MM-DD: ")
 
-requests = requests.get(f"https://www.billboard.com/charts/hot-100/{date}")
-requests = requests.text
-soup = BeautifulSoup(requests, "html.parser")
+date = "2000-12-02"
 
+response = requests.get("https://www.billboard.com/charts/hot-100/" + date)
+
+soup = BeautifulSoup(response.text, "html.parser")
+
+songs = soup.find_all(name="h3", id="title-of-a-story", class_="a-no-trucate")
+songs = [song.getText().strip("\n\t") for song in songs]
+
+for i in range(len(songs)):
+    print(songs[i])
