84322a59e2260d229c8c32f63bdb5d642098de05
Elvis <elvishenry2402@gmail.com>
2022-12-19 00:53:52 +0100

    Day48
    Ep. 6

--- Day48/main.py
+++ Day48/main.py
@@ -12,7 +12,23 @@ document_link = driver.find_element(By.CSS_SELECTOR, ".documentation-widget a")
 xpath = driver.find_element(By.XPATH, '//*[@id="site-map"]/div[2]/div/div/p/small/span[4]/a')
 
 #print(search.get_attribute("placeholder"))
-print(document_link.text)
-print(xpath.text)
+#print(document_link.text)
+#print(xpath.text)
+
+time = driver.find_elements(By.CSS_SELECTOR, ".event-widget time")
+text = driver.find_elements(By.CSS_SELECTOR, ".event-widget li a")
+
+event = {}
+
+for i in range(len(time)):
+    event[i] = {
+        "time": time[i].text,
+        "name": text[i].text
+    }
+
+document_link = driver.find_element(By.LINK_TEXT, "Documentation")
+document_link.click()
+
+#print(event)
 
 driver.quit()
