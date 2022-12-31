09e6f7b7edb464ceef65be40ffdb4fc1b70fc30a
Elvis <elvishenry2402@gmail.com>
2022-12-31 04:31:36 +0100

    Day51
    Done

--- Day51/main.py
+++ Day51/main.py
@@ -7,7 +7,7 @@ from selenium.common.exceptions import ElementClickInterceptedException, NoSuchE
 
 from time import sleep
 
-USERNAME = "@python12983740"
+USERNAME = "python12983740"
 PASSWORD = "pythontwitter"
 
 PROMISED_DOWN = 150
@@ -44,7 +44,36 @@ class InternetSpeedTwitterBot:
         print(self.up, self.down)
 
     def tweet_at_provider(self):
-        pass
+        self.driver.get("https://twitter.com/login")
+
+        sleep(2)
+
+        email = self.driver.find_element(By.XPATH, '//*[@id="react-root"]/div/div/div/main/div/div/div/div[2]/div[2]/div/div[5]/label/div/div[2]/div/input')
+        email.send_keys(USERNAME)
+
+        email.send_keys(Keys.ENTER)
+
+        sleep(1)
+
+        password = self.driver.find_element(By.XPATH, '//*[@id="react-root"]/div/div/div/main/div/div/div/div[2]/div[2]/div[1]/div/div/div/div[3]/div/label/div/div[2]/div[1]/input')
+        password.send_keys(PASSWORD)
+
+        sleep(0.5)
+        password.send_keys(Keys.ENTER)
+
+        sleep(5)
+        tweet_compose = self.driver.find_element(By.XPATH, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div[2]/div/div/div[2]/div[1]/div/div/div/div[2]/div[1]/div/div/div/div/div/div/div/div/div[1]/div/div/div/div/div/div/div/div/div/div')
+
+        tweet = f"Hey Internet Provider, why is my internet speed {self.down}down/{self.up}up when I pay for {PROMISED_DOWN}down/{PROMISED_UP}up?"
+
+        tweet_compose.send_keys(tweet)
+        sleep(3)
+
+        tweet_button = self.driver.find_element(By.XPATH, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div[2]/div/div/div[2]/div[1]/div/div/div/div[2]/div[4]/div/div/div[2]/div[4]/div/span/span')
+        tweet_button.click()
+
+        sleep(2)
+        self.driver.quit()
 
 bot = InternetSpeedTwitterBot()
 bot.get_internet_speed()
