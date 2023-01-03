d8f31c5f76a6c89971f8770bf55c71ce5bd17fd4
Elvis <elvishenry2402@gmail.com>
2023-01-03 21:49:17 +0100

    Day55
    Decorator

---
+++ Day55/main.py
@@ -0,0 +1,31 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+def make_bold(function):
+    def wrapper():
+        return f"<b>{function()}</b>"
+    return wrapper
+
+def make_emphasis(function):
+    def wrapper():
+        return f"<em>{function()}</em>"
+    return wrapper
+
+def make_underlined(function):
+    def wrapper():
+        return f"<u>{function()}</u>"
+
+    return wrapper
+
+@app.route('/')
+def hello_world():
+    return '<h1 style="text-align: center">Hello, World!</h1> \
+            <p>This is a paragraph.</p>'
+
+@app.route('/bye')
+@make_bold
+@make_emphasis
+@make_underlined
+def say_bye():
+    return "Bye"
