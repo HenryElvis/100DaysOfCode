ee36a5b03b1f5a374b6a3b73abc5498786e4dc53
Elvis <elvishenry2402@gmail.com>
2023-01-05 22:18:20 +0100

    Day56
    Template

---
+++ Day56/main.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+@app.route('/template')
+def template():
+    return render_template('index.html')
