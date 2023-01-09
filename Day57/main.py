b5909dea613bd6e385bbf0ac060d5aa2af4db860
Elvis <elvishenry2402@gmail.com>
2023-01-09 18:07:33 +0100

    Day57
    Multiline

--- Day57/main.py
+++ Day57/main.py
@@ -16,9 +16,15 @@ def guess(name):
     age_url = f"https://api.agify.io?name={name}"
 
     gender_response = requests.get(gender_url).json()
-    age_response = requests.get(age_url).json()["age"]
+    age_response = requests.get(age_url).json()
 
     gender = gender_response["gender"]
     age = age_response["age"]
     
     return render_template('guess.html', name=name.capitalize(), gender=gender, age=age)
+
+@app.route('/blog')
+def get_blog():
+    blog_url = "https://api.npoint.io/41c7c3f73a51845ba137"
+    response = requests.get(blog_url).json()
+    return render_template('blog.html', posts=response)
