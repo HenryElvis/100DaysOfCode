726364b2c3fa372d2940d88262ca146d14506fc6
Elvis <elvishenry2402@gmail.com>
2023-01-09 02:05:06 +0100

    Day57
    Guess

--- Day57/main.py
+++ Day57/main.py
@@ -1,6 +1,7 @@
 from random import randint
 from flask import Flask, render_template
 from datetime import date
+import requests
 
 app = Flask(__name__)
 
@@ -8,3 +9,16 @@ app = Flask(__name__)
 def hello_world():
     random = randint(1, 10)
     return render_template('index.html', rand=random, year=date.today().year)
+
+@app.route('/guess/<name>')
+def guess(name):
+    gender_url = f"https://api.genderize.io?name={name}"
+    age_url = f"https://api.agify.io?name={name}"
+
+    gender_response = requests.get(gender_url).json()
+    age_response = requests.get(age_url).json()["age"]
+
+    gender = gender_response["gender"]
+    age = age_response["age"]
+    
+    return render_template('guess.html', name=name.capitalize(), gender=gender, age=age)
