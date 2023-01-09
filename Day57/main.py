860a576d9a8e7c1becd742650068cee4cf1cffcc
Elvis <elvishenry2402@gmail.com>
2023-01-09 01:35:48 +0100

    Day57
    Render a variable

--- Day57/main.py
+++ Day57/main.py
@@ -1,7 +1,10 @@
+from random import randint
 from flask import Flask, render_template
+from datetime import date
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
+    random = randint(1, 10)
+    return render_template('index.html', rand=random, year=date.today().year)
