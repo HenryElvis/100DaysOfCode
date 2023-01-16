a07f6888fa8c4046b11b680e5f55096a06b22afb
Elvis <elvishenry2402@gmail.com>
2023-01-16 21:46:00 +0100

    Day59
    Done

--- Day59/main.py
+++ Day59/main.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
+API = "https://api.npoint.io/b4864fe025890c73f872"
+
+posts = requests.get(API).json()
+
 @app.route('/')
 def Home():
-    return render_template('index.html')
+    return render_template('index.html', all_posts=posts)
 
 @app.route('/about')
 def About():
@@ -13,3 +18,11 @@ def About():
 @app.route('/contact')
 def Contact():
     return render_template('contact.html')
+
+@app.route('/post/<int:index>')
+def get_post(index):
+    requested_post = None
+    for blog_post in posts:
+        if blog_post["id"] == index:
+            requested_post = blog_post
+    return render_template('post.html', post=requested_post)
