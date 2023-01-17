c1999c3bdf9f2d3436f474eeadf67ed460e8d8ab
Elvis <elvishenry2402@gmail.com>
2023-01-17 21:19:18 +0100

    Day60
    Done

--- Day60/main.py
+++ Day60/main.py
@@ -1,7 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def Home():
     return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def Receive_Data():
+    name = request.form['username']
+    password = request.form['password']
+    return f"<h1>Name: {name}, Password: {password}</h1>"
