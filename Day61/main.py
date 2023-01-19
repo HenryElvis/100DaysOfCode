a2eb255ba023502279105b1614984d14bce0ced7
Elvis <elvishenry2402@gmail.com>
2023-01-19 02:11:10 +0100

    Day61

--- Day61/main.py
+++ Day61/main.py
@@ -2,21 +2,17 @@ from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 
-
 class LoginForm(FlaskForm):
     email = StringField(label='Email')
     password = PasswordField(label='Password')
     submit = SubmitField(label="Log In")
 
-
 app = Flask(__name__)
 app.secret_key = "any-string-you-want-just-keep-it-secret"
-
 
 @app.route("/")
 def home():
     return render_template("index.html")
-
 
 @app.route("/login")
 def login():
