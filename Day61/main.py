5ee9aad997cd37ff0c14d11b6b5891f6363b0949
Elvis <elvishenry2402@gmail.com>
2023-01-19 14:41:32 +0100

    Day61
    Validator

--- Day61/main.py
+++ Day61/main.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
-    email = StringField(label='Email')
-    password = PasswordField(label='Password')
+    email = StringField(label='Email', validators=[DataRequired()])
+    password = PasswordField(label='Password', validators=[DataRequired()])
     submit = SubmitField(label="Log In")
 
 app = Flask(__name__)
@@ -14,7 +15,7 @@ app.secret_key = "any-string-you-want-just-keep-it-secret"
 def home():
     return render_template("index.html")
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
     login_form = LoginForm()
     return render_template('login.html', form=login_form)
