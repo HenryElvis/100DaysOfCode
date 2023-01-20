be597156f8cf7140ede31e69a1a07c738adb6d78
Elvis <elvishenry2402@gmail.com>
2023-01-20 01:07:25 +0100

    Day61
    Done

--- Day61/main.py
+++ Day61/main.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
+from flask_bootstrap import Bootstrap
 
 class LoginForm(FlaskForm):
     email = StringField(label='Email', validators=[DataRequired()])
@@ -10,6 +11,7 @@ class LoginForm(FlaskForm):
 
 app = Flask(__name__)
 app.secret_key = "any-string-you-want-just-keep-it-secret"
+Bootstrap(app)
 
 @app.route("/")
 def home():
@@ -18,4 +20,9 @@ def home():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     login_form = LoginForm()
+    if login_form.validate_on_submit():
+        if login_form.email.data == "admin@email.com" and login_form.password.data == "12345678":
+            return render_template("success.html")
+        else:
+            return render_template("denied.html")
     return render_template('login.html', form=login_form)
