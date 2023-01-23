fa8071d4e8dd069b919c0b1dc58c9863d05ad8fe
Elvis <elvishenry2402@gmail.com>
2023-01-23 18:08:25 +0100

    Day63
    Start db

--- Day63/main.py
+++ Day63/main.py
@@ -1,20 +1,31 @@
 from flask import Flask, render_template, request, redirect, url_for
+import sqlite3
 
 app = Flask(__name__)
 
 all_books = []
 
+db = sqlite3.connect("books-collection.db")
+cursor = db.cursor()
+
 
 @app.route('/')
 def home():
-    pass
+    return render_template("index.html", books=all_books)
 
-
-@app.route("/add")
+@app.route("/add", methods=["GET", "POST"])
 def add():
-    pass
+    if request.method == "POST":
+        new_book = {
+            "title": request.form["title"],
+            "author": request.form["author"],
+            "rating": request.form["rating"]
+        }
 
+        all_books.append(new_book)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+        return redirect(url_for("home"))
+    
+    return render_template("add.html")
 
+print(all_books)
